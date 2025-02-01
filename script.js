c819e94bdd943635979e357ca5db40bc3b164436
Armaan67 <117089919+Armaan67@users.noreply.github.com>
2025-02-01 16:49:01 -0500

    Functional product with mode and random color

--- script.js
+++ script.js
@@ -2,24 +2,143 @@ const container = document.querySelector(".container");
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
 container.style.padding = "0";
-container.style.height = "50vh";
-container.style.width = "50vh"
+container.style.height = "65vh";
+container.style.width = "65vh"
 
 
-for (let i = 0; i<256; i++) {
-    const square = document.createElement("div");
-    square.style.height = "6.25%";
-    square.style.backgroundColor = "black";
-
-    square.style.boxSizing = "border-box";
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
     
-    square.style.flex = "1 1 6.25%";
 
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "blue";
+function addSquares(size) {
+    for (let i = 0; i<size*size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        let dimension = 100/size
+        square.style.height = dimension.toString() + "%";
+        square.style.backgroundColor = "black";
+    
+        square.style.boxSizing = "border-box";
+        
+        square.style.flexGrow = "1";
+        square.style.flexShrink = "1";
+        square.style.flexBasis = dimension.toString() + "%";
 
+        square.addEventListener("mousedown", () => {
+            if (modeButtonState) {
+                if (colorButtonState) {
+                    square.style.backgroundColor = getRandomColor()
+                } else {
+                    square.style.backgroundColor = colorPicker.value;
+                }
+            }
+
+        })
+
+        square.addEventListener("mouseenter", () => {
+            if (!modeButtonState) {
+                if (colorButtonState) {
+                    square.style.backgroundColor = getRandomColor()
+                } else {
+                    square.style.backgroundColor = colorPicker.value;
+                }
+            }
+        }) 
+
+        
+
+     
+        
+        
+        container.appendChild(square)
+    
+    };
+}
+
+const modeButton = document.querySelector(".mode")
+let modeButtonState = false;
+
+modeButton.addEventListener("click", () =>{
+    modeButtonState = !modeButtonState
+
+    if (modeButtonState) {
+        modeButton.style.backgroundColor = "#7b5b4a";
+        modeButton.style.color = "#faf3e0";
+    } else {
+        modeButton.style.backgroundColor = "#faf3e0";
+        modeButton.style.color = "#7b5b4a";
+    }
+
+})
+
+
+addSquares(16);
+
+
+const clearbutton = document.querySelector(".clearbutton");
+
+clearbutton.addEventListener("click", () => {
+    squares = document.querySelectorAll(".square")
+    squares.forEach((item) => {
+        item.style.backgroundColor = "black"
+    })
+
+
+})
+
+
+
+const gridbutton = document.querySelector(".grid-size")
+gridbutton.addEventListener("click", () => {
+    squares = document.querySelectorAll(".square")
+
+    let gridSize = parseInt(prompt("Enter Grid Size (Max: 100)"));
+    while (isNaN(gridSize) || gridSize > 100) {
+        alert("Enter valid grid size! (Max: 100)");
+        gridSize = parseInt(prompt("Enter Grid Size"))
+
+    };
+
+    squares.forEach((square) => {
+        square.remove()
     });
-    
-    container.appendChild(square)
 
-};
+    addSquares(gridSize);
+
+})
+
+
+const colorButton = document.querySelector(".color");
+const colorPicker = document.querySelector(".color .colorpicker")
+
+let colorButtonState = false; // false = off, true = on (random color)
+
+colorButton.addEventListener("click", () => {
+    colorButtonState = !colorButtonState
+
+    if (colorButtonState) {
+        colorButton.style.backgroundColor = "#7b5b4a";
+        colorButton.style.color = "#faf3e0";
+        colorPicker.style.backgroundColor = "#7b5b4a"; // make this constantly changing to show random colors being generated
+
+    } else {
+        colorButton.style.backgroundColor = "#faf3e0";
+        colorButton.style.color = "#7b5b4a";
+        colorPicker.style.backgroundColor = "#faf3e0"; 
+    }
+
+});
+
+colorPicker.addEventListener("click", (event) => {
+    event.stopPropagation()
+    
+    
+})
+
+
